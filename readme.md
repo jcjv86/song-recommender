d1beaac6578adcd560fbe48e495290b818a2895b
Juan Jimenez <jcjv86@gmail.com>
2023-02-17 12:30:46 +0100

    Final version of song recommender

---
+++ readme.md
@@ -0,0 +1,23 @@
+![Alt text](src/img/logo.png?raw=true "Title")
+
+ #### The song recommender
+
+## Opening your ears to a wide world of music
+
+Tired of listening always to the same music?*
+
+**TRY Wide!**
+
+## *What makes us different?*
+
+1. **Precision**
+With a vast library of songs across all genres, our app offers a personalized music experience that tailors recommendations to your unique preferences.
+
+2. **Spotify link of your recommendation**
+We provide a direct link of the recommendation so you can listen  right away your future favorite songs
+
+3. **Choose the number of recommendations**
+With Wide you can choose the amount of recommendations you want. You can have one track or an entire list.
+
+4. **Features layout**
+We know that is important to understand what features you love of your music. Thats why we integrate a plot to show you what are the things  you love.
--- src/lib/functions.py
+++ src/lib/functions.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import sys
+import os
+import warnings
+warnings.simplefilter('ignore')
 sys.path.insert(1, '/home/juan/Documents/')
 import time
 import pickle
@@ -11,20 +14,31 @@ import spotipy
 import json
 from spotipy.oauth2 import SpotifyClientCredentials
 from IPython.display import HTML
+import matplotlib.pyplot as plt
+import pandas as pd
+from math import pi
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= client_id,
                                                            client_secret= client_secret))
 
 def search_song():
-    title = str(input('Please enter song title: ')).lower()
-    artist = str(input('Please enter artist name: ')).lower()
+    title = str(input('Please enter song title: \n\n')).lower()
+    artist = str(input('\nPlease enter artist name: \n\n')).lower()
     print()
+    
     try:
         results = sp.search(q=title+','+artist, limit=5)
-        #song_id = results['tracks']['items'][-1]['id']
-        return results
     except: 
         return 'na'
+    while len(results['tracks']['items'])<5:
+        title = str(input('Sorry not found. Please enter song title again: \n\n')).lower()
+        artist = str(input('\nPlease enter artist name: \n\n')).lower()
+        try:
+            results = sp.search(q=title+','+artist, limit=5)
+        except: 
+            return 'na'
+    return results
+    
 
 def user_select(results):
     base_url = 'https://open.spotify.com/track/'
@@ -40,11 +54,12 @@ def user_select(results):
         songs.append(results['tracks']['items'][index]['id'])
         counter += 1
         index += 1
-        
+    
     user_selection = str(input('\n\nWhich song would you prefer? Please enter a number from the list above: \n'))
     valid_values = [str(i) for i in range (1, len(results['tracks']['items'])+1)]
+    
     while user_selection not in valid_values:
-        user_selection = str(input('Invalid value. Please enter Please enter a number in the list!: \n'))
+        user_selection = str(input('Invalid value. Please enter a number in the list!: \n\n'))
     print()
     if user_selection == '1':
         return songs[0]
@@ -56,7 +71,7 @@ def user_select(results):
         return songs[3]
     elif user_selection == '5':
         return songs[4]
-
+    
 
 def get_audio_features(song_id):
     my_dict = {}
@@ -93,16 +108,16 @@ def clustering(X_scaled_df):
 def make_clickable(link):
     return '<a href="{0}">{0}</a>'.format(link)
 
-def selector(final_scaled, database, song_id, final):
+def selector(final_scaled, database, song_id, final, df2, selection):
     
     song_recomender_df = pd.concat([database[['title','artist','id','source']],final_scaled],axis=1)
     song_recomender_df_hot = song_recomender_df[(song_recomender_df['source'] == 'H')]
     song_recomender_df_nonhot = song_recomender_df[(song_recomender_df['source'] == 'N')]
     
     if song_id in song_recomender_df_hot:
-        song_recomender_test = song_recomender_df_hot[(song_recomender_df_hot['cluster'] == final)].sample(5)
+        song_recomender_test = song_recomender_df_hot[(song_recomender_df_hot['cluster'] == final)].sample(selection)
     else:
-        song_recomender_test = song_recomender_df_nonhot[(song_recomender_df_nonhot['cluster'] == final)].sample(5)
+        song_recomender_test = song_recomender_df_nonhot[(song_recomender_df_nonhot['cluster'] == final)].sample(selection)
     
     song_recomender_test = song_recomender_test[['title','artist', 'id']]
     base_url = 'https://open.spotify.com/track/'
@@ -114,9 +129,47 @@ def selector(final_scaled, database, song_id, final):
     
     song_recomender_test = song_recomender_test.drop(['id'], axis=1)
     print()
-    print('Here are 5 similar songs to your selection: \n')
+    print('Displaying', selection, 'similar song(s) to your selection: \n')
     display(HTML(song_recomender_test.to_html(escape=False)))
+    print('\n\n')
+    
+    #Graph
+    df2 = df2[['danceability','energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness',
+       'valence']]
 
+    categories=list(df2)[0:]
+    N = len(categories)
+ 
+    values = df2.loc[0].values.flatten().tolist()
+    values += values[:1]
+    values
+ 
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
+ 
+
+    ax = plt.subplot(111, polar=True)
+ 
+
+    plt.xticks(angles[:-1], categories, color='grey', size=8)
+ 
+
+    ax.set_rlabel_position(0)
+    plt.yticks([0.20,0.40,0.60,0.8], ['0.20','0.40','0.60','0.8'], color="grey", size=7)
+    plt.ylim(0,1)
+ 
+    # Plot data
+    ax.plot(angles, values, linewidth=1, linestyle='solid', color = 'm')
+ 
+    # Fill area
+    ax.fill(angles, values, 'm', alpha=0.1)
+
+    # Show the graph
+    print('These are the features of the song you like!\n')
+    plt.tight_layout()
+    plt.show()
+    
+    
 
 def song_recommender(df):
     #Get song id
@@ -127,14 +180,29 @@ def song_recommender(df):
     song_id = user_select(results)
     
     #Get audio features of the song and display them
-    df = get_audio_features(song_id)
+    df2 = get_audio_features(song_id)
     
     #Scale song feat by adding it to our database and then returning it scaled.
-    df2 = database[['danceability','energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness',
+    df3 = database[['danceability','energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness',
        'valence', 'tempo']]
-    df3 = pd.concat([df2,df],axis=0)
-    df_scaled = scaler(df3)
+    df4 = pd.concat([df3,df2],axis=0)
+    df_scaled = scaler(df4)
     final = clustering(df_scaled)
 
     final_scaled = df_scaled.iloc[0:-1]
-    selector(final_scaled, database, song_id, final)
+    try:
+        selection = int(input('\nHow many songs do you want us to recommend you? Maximum 20!\n'))
+        if selection > 20:
+            selection = 20
+    except:
+        print('\nInvalid value.\n')
+        selection = 5
+    selector(final_scaled, database, song_id, final, df2, selection)
+    print('\n\nThis is the explanation of the features. They are measured on a scale from 0 to 1:\n')
+    print('-Energy: represents how intense and active the song is.')
+    print('-Danceability: describes how suitable a track is for dancing.')
+    print('-Valence: a measure describing the musical positiveness. The higher, the more cheerful.')
+    print('-Liveness: detects if the track was recorded live.')
+    print('-Instrumentalness: track contains no vocals.')
+    print('-Acousticness: possibility of the track being acoustic.')
+    print('-Speechiness: Speechiness detects the presence of spoken words in a track.')
